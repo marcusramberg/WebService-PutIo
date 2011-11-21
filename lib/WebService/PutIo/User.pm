7cbba13a0605e247779b5ede98ae28ad10d8a233
Marcus Ramberg <marcus@nordaaker.com>
2011-11-21 08:34:43 +0100

    Add some pod

--- lib/WebService/PutIo/User.pm
+++ lib/WebService/PutIo/User.pm
@@ -7,6 +7,7 @@ my $class='user';
 sub info { shift->request($class,'info',@_); }
 sub friends { shift->request($class,'friends',@_); }
 sub token { shift->request($class,'acctoken',@_); }
+
 =head1 NAME
 
 WebService::PutIo::URLs - Analyze URLs
@@ -33,6 +34,11 @@ Returns more detailed info about the user.
 =head2 friends
 
 Returns user's friend list.
+
+=head2 token
+
+Returns user's access token.
+
 
 =head1 COPYRIGHT AND LICENSE
 
